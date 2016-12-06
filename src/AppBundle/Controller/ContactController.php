f1d7b8d0a598653511f39abd4d09eeb780c35eea
sdede <s.dede@laposte.net>
2016-12-06 15:34:25 +0100

    add contactController

--- src/AppBundle/Controller/ContactController.php
+++ src/AppBundle/Controller/ContactController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Model\Product;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 
 class ContactController extends Controller
 {
@@ -14,20 +15,22 @@ class ContactController extends Controller
     * @Route("/{_locale}/contact", name="contact")
     */
     public function contactAction(){
-        dump("ok");
         return $this->render('hangman/contact.html.twig');
     }
 
     /**
      * @Route("/product", name="product")
      */
-    public function productAction(){
+    public function productAction(Request $request){
         $product = new Product();
         $product->name = 'toto';
         $product->setPrice(50.0);
         $form = $this->createForm(ProductType::class, $product);
         $form->add('send',SubmitType::class);
-        return $this->render('hangman/product.html.twig',[
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            return $this->render('hangman/product.html.twig',[
             'formView'=> $form->createView()
 
         ]);
